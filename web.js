b5d3897aa00fc9e0b515299ddad6310cfcbbc3f8
Michael Keenan <michael.keenan@gmail.com>
2013-07-31 02:21:04 +0000

    We now read content from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFile('/index.html', function (err, data) {
+    if (err) throw err;
+    response.send(data.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
